81b1167fda1337f699cfded6fa0522e313e5a01c
anoops1997 <anoopspillaimgd@gmail.com>
2023-12-02 15:42:28 +0530

    modified:   app.py

--- app.py
+++ app.py
@@ -1,27 +1,50 @@
 import gradio as gr
 import tensorflow as tf
 import numpy as np
+from PIL import Image
 
 # Load the pre-trained MNIST model
 model = tf.keras.models.load_model('mnist_model.h5')
 
 # Function to make predictions
-def predict_image(img):
-    # Preprocess the input image
-    img = img.reshape((28, 28, 1)).astype('float32') / 255.0
+def predict_image(drawing):
+    try:
+        # Get the drawing as a NumPy array
+        img_array = drawing.image
 
-    # Make predictions
-    predictions = model.predict(np.expand_dims(img, axis=0))
+        # Resize the input image to (28, 28)
+        pil_image = Image.fromarray((img_array * 255).astype('uint8'))
+        pil_image = pil_image.resize((28, 28))
 
-    # Get the predicted class
-    predicted_class = np.argmax(predictions[0])
+        # Convert the image to grayscale
+        pil_image = pil_image.convert("L")
 
-    return f"Predicted Digit: {predicted_class}"
+        # Convert the image to a numpy array
+        img_array = np.array(pil_image)
 
-# Create the Gradio interface
+        # Normalize the pixel values to the range [0, 1]
+        img_array = img_array.astype('float32') / 255.0
+
+        # Ensure the array has the correct shape
+        if img_array.shape == (28, 28):
+            img_array = np.expand_dims(img_array, axis=-1)
+
+            # Make predictions
+            predictions = model.predict(np.expand_dims(img_array, axis=0))
+
+            # Get the predicted class
+            predicted_class = np.argmax(predictions[0])
+
+            return f"Predicted Digit: {predicted_class}"
+        else:
+            return "Error: Invalid image size after resizing"
+    except Exception as e:
+        return f"Error during prediction: {str(e)}"
+
+# Create the Gradio interface with a drawing input
 iface = gr.Interface(
     fn=predict_image,
-    inputs="image",
+    inputs=gr.Drawing(allow_clear=True),
     outputs="text",
 )
 
