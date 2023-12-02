22720c4b4818548af41b9df07465f173aea96b47
anoops1997 <anoopspillaimgd@gmail.com>
2023-12-02 15:14:25 +0530

    updated app.py

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
 import gradio as gr
 import tensorflow as tf
-from keras.preprocessing import image
-from keras.applications.inception_v3 import preprocess_input
 import numpy as np
 
 # Load the pre-trained MNIST model
@@ -10,30 +8,21 @@ model = tf.keras.models.load_model('mnist_model.h5')
 # Function to make predictions
 def predict_image(img):
     # Preprocess the input image
-    img_array = image.img_to_array(img)
-    img_array = np.expand_dims(img_array, axis=0)
-    img_array /= 255.0
+    img = img.reshape((28, 28, 1)).astype('float32') / 255.0
 
     # Make predictions
-    predictions = model.predict(img_array)
+    predictions = model.predict(np.expand_dims(img, axis=0))
 
     # Get the predicted class
     predicted_class = np.argmax(predictions[0])
 
     return f"Predicted Digit: {predicted_class}"
 
-# Define the input component
-input_component = gr.Image(preprocessing_fn=preprocess_input, shape=(28, 28, 1))
-
 # Create the Gradio interface
 iface = gr.Interface(
     fn=predict_image,
-    inputs=input_component,
+    inputs="image",
     outputs="text",
-    live=True,
-    theme="light",
-    capture_session=True,
-    interpretation="default"
 )
 
 # Launch the Gradio interface
